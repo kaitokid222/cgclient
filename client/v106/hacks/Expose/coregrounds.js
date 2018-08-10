b284832e2d0d6d72581eba942b0de71a476192df
Kaito <33453552+kaitokid222@users.noreply.github.com>
2018-08-10 23:00:05 +0200

    added icons

--- client/v106/hacks/Expose/coregrounds.js
+++ client/v106/hacks/Expose/coregrounds.js
@@ -1,11 +1,12 @@
 (0, function(){
 	//whole coregrounds.js minified
-	window.cg = {
-		config : Config,
-		event : Event,
-		screens : Screens,
-		game : Game,
-		lobbyserver : LobbyServer,
-		gameserver : GameServer
-	};
+window.cg = {
+    config : Config,
+    event : Event,
+    screens : Screens,
+	game : Game,
+	lobbyserver : LobbyServer,
+	gameserver : GameServer,
+	icons : Icons
+};
 });
