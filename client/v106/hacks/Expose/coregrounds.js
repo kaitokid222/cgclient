ae72fcaef10c6b52542c1f66ed65b93e461278af
Kaito <33453552+kaitokid222@users.noreply.github.com>
2018-08-10 22:21:43 +0200

    expose vars in global scope

--- client/v106/hacks/Expose/coregrounds.js
+++ client/v106/hacks/Expose/coregrounds.js
@@ -1,4 +1,11 @@
 (0, function(){
 	//whole coregrounds.js minified
-	
+	window.cg = {
+		config : Config,
+		event : Event,
+		screens : Screens,
+		game : Game,
+		lobbyserver : LobbyServer,
+		gameserver : GameServer
+	};
 });
