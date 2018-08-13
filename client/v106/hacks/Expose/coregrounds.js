49980e1cc2f908eabd8d3e5b29743da400deef67
Shurutsue <38896135+Shurutsue@users.noreply.github.com>
2018-08-13 02:08:50 +0200

    Added tooltips for Upgrade plugin. Dependency of expose hack.

--- client/v106/hacks/Expose/coregrounds.js
+++ client/v106/hacks/Expose/coregrounds.js
@@ -1,12 +1,14 @@
 (0, function(){
 	//whole coregrounds.js minified
-window.cg = {
-    config : Config,
-    event : Event,
-    screens : Screens,
-	game : Game,
-	lobbyserver : LobbyServer,
-	gameserver : GameServer,
-	icons : Icons
-};
+  window.cg = {
+    config: Config,
+    event: Event,
+    screens: Screens,
+    game: Game,
+    lobbyserver: LobbyServer,
+    gameserver: GameServer,
+    icons: Icons,
+    message: Messages,
+    upgradebuttons: upgradeBtns
+  };
 });
